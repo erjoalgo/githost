d6cab07881897ceb7d5b42328cf6b80125e92399
Ernesto Alfonso <erjoalgo@gmail.com>
2022-03-30 01:43:12 -0400

    debugging: attempt to enable trace logging of http requests

--- githost/githost.py
+++ githost/githost.py
@@ -124,6 +124,7 @@ class Service(object):
         if add_auth:
             self.req_auth(req)
         resp = requests.Session().send(req.prepare())
+        print(req.headers)
         if not resp.ok:
             print (resp.text)
             resp.raise_for_status()
@@ -293,7 +294,16 @@ def main():
         return 0
     args = parser.parse_args()
 
-    logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
+    try: # for Python 3
+        from http.client import HTTPConnection
+    except ImportError:
+        from httplib import HTTPConnection
+        HTTPConnection.debuglevel = 1
+    level = logging.DEBUG if args.verbose else logging.INFO
+    logger.setLevel(level)
+    requests_log = logging.getLogger("urllib3")
+    requests_log.setLevel(level)
+    requests_log.propagate = True
 
     auth = Auth(user=args.username, authinfo=args.authinfo)
     service_fn = SERVICES.get(args.service)
